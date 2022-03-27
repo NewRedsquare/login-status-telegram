32a64a7509d5210c02c36d33f7c53c205653bb77
NewRedsquare <38375491+NewRedsquare@users.noreply.github.com>
2022-03-28 01:36:42 +0200

    Add IPv6 compatibility

--- telegram_bot.sh
+++ telegram_bot.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 HOSTNAME1="sh -c 'host $PAM_RHOST' | sed 's/^.* * * * //'"
-HOSTNAME_OUTPUT=$(eval "$HOSTNAME1")
-CITY="curl ipinfo.io/$PAM_RHOST | jq '.city' | sed 's/\"//g'"
-CITY1=$(eval "$CITY")
-REGION="curl ipinfo.io/$PAM_RHOST | jq '.region' | sed 's/\"//g'"
-REGION1=$(eval "$REGION")
-COUNTRY="curl ipinfo.io/$PAM_RHOST | jq '.country' | sed 's/\"//g'"
-COUNTRY1=$(eval "$COUNTRY")
-ORG="curl ipinfo.io/$PAM_RHOST | jq '.org' | sed 's/\"//g'"
-ORG1=$(eval "$ORG")
-TOKEN=""
-URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+ACCESS_TOKEN=""
+URL="https://api.telegram.org/bot$ACCESS_TOKEN/sendMessage"
 CHAT_ID="" # Telegram id from /getUpdates
+if [[ $PAM_RHOST =~ .*:.* ]] ; then
+   REGION=`geoiplookup6 $PAM_RHOST | sed -n '3p' | awk -v N=8 '{print $N}' | sed 's/,//'`
+   COUNTRY=`geoiplookup6 $PAM_RHOST | sed -n '1p' | grep ', ' | sed 's/^.*, //'`
+   ORG=`geoiplookup6 $PAM_RHOST | sed -n '2p' | sed -n 's/^.*: AS/AS/p'`
+else
+   REGION=`geoiplookup $PAM_RHOST | sed -n '2p' | awk -v N=7 '{print $N}' | sed 's/,//'`
+   COUNTRY=`geoiplookup $PAM_RHOST | sed -n '1p' | grep ', ' | sed 's/^.*, //'`
+   ORG=`geoiplookup $PAM_RHOST | sed -n '3p' | sed -n 's/^.*: AS/AS/p'`
+fi
 MESSAGE="*Login Alert*: $(date '+%Y-%m-%d %H:%M:%S %Z')
  username: $PAM_USER
  hostname: $HOSTNAME
  remote host: $PAM_RHOST
- remote hostname : $HOSTNAME_OUTPUT
- city : $CITY1
- region : $REGION1
- country : $COUNTRY1
- ISP : $ORG1
+ region : $REGION
+ country : $COUNTRY
+ ISP : $ORG
  service: $PAM_SERVICE
  tty: $PAM_TTY"
 PAYLOAD="chat_id=$CHAT_ID&text=$MESSAGE&disable_web_page_preview=true&parse_mode=Markdown"
